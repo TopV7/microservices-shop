ee7678dab0b43e109492dd584f419e3f82c07f30
Eduard FX <muftakhoved@gmail.com>
2026-01-28 21:06:13 +0500

    Implement product listing logic and update file encoding

--- catalog-service/server.py
+++ catalog-service/server.py
@@ -1,1 +1,6 @@
-print("Catalog Service is starting...")
+
+with open("products.txt", "r", encoding="utf-8") as file:
+    print("--- Catalog Service Products ---")
+    for line in file:
+        print(line.strip())
+
